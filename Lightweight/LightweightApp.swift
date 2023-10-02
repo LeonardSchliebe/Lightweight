b2562655d39db61b599a7f08503eb01d555b8ca7
LeonardSchliebe <leonardcarl.schliebe@gmail.com>
2023-10-02 21:45:03 +0200

    Initial Commit

---
+++ Lightweight/LightweightApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightweightApp.swift
+//  Lightweight
+//
+//  Created by Leonard Carl Schliebe on 02.10.23.
+//
+
+import SwiftUI
+
+@main
+struct LightweightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
